428259a24a4e7e83b7db3fa6c06150ae68db9df9
lachezar-atanasov <lachezar.atanasov99@gmail.com>
2024-06-28 00:13:06 +0300

    Test Input added to StartUp Main

---
+++ P03_FootballBetting/StartUp.cs
@@ -0,0 +1,85 @@
+﻿using P03_FootballBetting.Data;
+using P03_FootballBetting.Data.Models;
+
+namespace P03_FootballBetting
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var context = new FootballBettingContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var redColor = new Color()
+            {
+                Name = "Red"
+            };
+
+            context.Bets.Add(
+                new Bet()
+                {
+                    Amount = 100.0d,
+                    DateTime = DateTime.Now,
+                    Game = new Game()
+                    {
+                        AwayTeam = new Team()
+                        {
+                            Budget = 10000m,
+                            Initials = "JUV",
+                            LogoUrl = "http://google.com/",
+                            PrimaryKitColor = redColor,
+                            SecondaryKitColor = new Color()
+                            {
+                                Name = "Blue"
+                            },
+                            Name = "Juventus",
+                            Town = new Town()
+                            {
+                                Country = new Country()
+                                {
+                                    Name = "Spain"
+                                }
+                            }
+                        },
+                        HomeTeam = new Team()
+                        {
+                            Budget = 20000m,
+                            Initials = "LIV",
+                            LogoUrl = "http://google.com/",
+                            PrimaryKitColor = redColor,
+                            SecondaryKitColor = new Color()
+                            {
+                                Name = "Yellow"
+                            },
+                            Name = "Liverpool",
+                            Town = new Town()
+                            {
+                                Country = new Country()
+                                {
+                                    Name = "Bulgaria"
+                                }
+                            }
+                        },
+                        AwayTeamBetRate = 1.45,
+                        DrawBetRate = 4.5,
+                        HomeTeamBetRate = 3.45,
+                        AwayTeamGoals = 3,
+                        HomeTeamGoals = 6,
+                        DateTime = DateTime.Now,
+                        Result = "1/1"
+                    },
+                    User = new User()
+                    {
+                        Balance = 100.0m,
+                        Email = "luchi507@abv.bg",
+                        Name = "Lachezar",
+                        Password = "NoPassword",
+                        Username = "Bolonkata13"
+                    },
+                    Prediction = 1
+                });
+            context.SaveChanges();
+        }
+    }
+}
